3d8fd019c20721c31fb45cd990e1eba88e3cfade
Gojeongtae <giwigi@naver.com>
2022-03-17 12:34:47 +0900

    교수님 주신 과제. State변환에 대한 시간을 주도록 timer설정

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -13,49 +13,87 @@ public class Monster : FSM
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (currentState == State.WALK && Input.GetMouseButtonDown(0))
         {
+            isNewState = true;
             SetState(State.CHASE);
         }
-        else if (Input.GetMouseButtonDown(1))
+        else if (currentState == State.CHASE && Input.GetMouseButtonDown(1))
         {
+            isNewState = true;
             SetState(State.ATTACK);
         }
     }
 
     protected override IEnumerator IDLE()
     {
-        
-        Debug.Log("Player" + currentState + "ING");
-        new WaitForSeconds(3f);
-        Debug.Log("Player" + currentState + "Done");
-        SetState(State.WALK);
-        yield return null; 
+        while(isNewState == false)
+        {
+            Debug.Log("Player" + currentState + "ING");
+            yield return new WaitForSeconds(7f);
+            Debug.Log("Player" + currentState + "Done");
+            SetState(State.WALK);
+            yield return null;
+        }
+       
+       
     }
     protected override IEnumerator WALK()
     {
+        float timer = 0f;
+
         while (isNewState == false)
         {
-            Debug.Log("Player" + currentState + "ING");
+            timer += Time.deltaTime;
+
+            if(timer >= 7f)
+            {
+                Debug.Log("Player" + currentState + "DONE");
+                SetState(State.IDLE);
+            }
+            yield return null;
         }
-        yield return null;
     }
     protected override IEnumerator CHASE()
     {
-        Debug.Log("Player" + currentState + "ING");
-        yield return new WaitForSeconds(3f);
-        Debug.Log("Player" + currentState + "Done");
-        SetState(State.WALK);
-        yield return null;
+        Debug.Log("Player" + currentState + "Start");
+        float timer = 0f;
+
+        while (isNewState == false)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= 7f)
+            {
+                Debug.Log("Player" + currentState + "DONE");
+                SetState(State.WALK);
+            }
+            yield return null;
+        }
     }
     protected override IEnumerator ATTACK()
     {
         while (isNewState == false)
         {
             Debug.Log("Player" + currentState + "ING");
+            yield return new WaitForSeconds(1f);
+            Debug.Log("Player" + currentState + "ING");
+            yield return new WaitForSeconds(1f);
+            Debug.Log("Player" + currentState + "ING");
+            yield return new WaitForSeconds(1f);
+            Debug.Log("Player" + currentState + "ING");
+            yield return new WaitForSeconds(1f);
+            Debug.Log("Player" + currentState + "ING");
+            yield return new WaitForSeconds(1f);
+            Debug.Log("Player" + currentState + "ING");
+            yield return new WaitForSeconds(1f);
+            Debug.Log("Player" + currentState + "ING");
+            yield return new WaitForSeconds(1f);
+            Debug.Log("Player" + currentState + "Done");
+            SetState(State.DEAD);
+            yield return null;
         }
-        SetState(State.WALK);
-        yield return null;
+
     }
     protected override IEnumerator DEAD()
     {
